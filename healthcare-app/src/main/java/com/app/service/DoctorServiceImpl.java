29ca77fe56add45a7f1b7bc657db14d7fe22ded9
D6-80621-Kushagra-Khatri <144333385+kushagrakhatri@users.noreply.github.com>
2024-02-16 14:15:15 +0530

    doctor controller, service part 2

--- healthcare-app/src/main/java/com/app/service/DoctorServiceImpl.java
+++ healthcare-app/src/main/java/com/app/service/DoctorServiceImpl.java
@@ -1,12 +1,15 @@
 package com.app.service;
 
 import com.app.dao.DoctorDao;
+import com.app.dao.GroupDao;
 import com.app.dao.JobDao;
 import com.app.dto.DoctorPutDTO;
 import com.app.dto.GroupDTO;
 import com.app.entities.Doctor;
+import com.app.entities.Group;
 import com.app.entities.Job;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +26,12 @@ public class DoctorServiceImpl implements DoctorService{
 
     @Autowired
     DoctorDao doctorDao;
+
+    @Autowired
+    GroupDao groupDao;
+
+    @Autowired
+    ModelMapper mapper;
     @Override
     public List<Job> viewJobs(Long docId) {
         return jobDao.findAll();
@@ -47,36 +56,44 @@ public class DoctorServiceImpl implements DoctorService{
 
     @Override
     public void withdrawApplication(Long docId, Long jobId) {
-
+        doctorDao.getReferenceById(docId).getJobsApplied().remove(jobDao.getReferenceById(jobId));
+        jobDao.getReferenceById(jobId).getDoctorsApplied().remove(doctorDao.getReferenceById(docId));
     }
 
     @Override
     public Doctor viewProfile(Long docId) {
-        return null;
+        return doctorDao.findById(docId).orElseThrow();
     }
 
     @Override
     public Doctor updateProfile(Long docId, DoctorPutDTO newDoctorDetails) {
-        return null;
+        Doctor doctorNew = mapper.map(newDoctorDetails, Doctor.class);
+        doctorNew.setId(docId);
+        return doctorNew;
     }
 
     @Override
     public void createGroup(Long docId, GroupDTO groupDTO) {
-
+        Group group = mapper.map(groupDTO, Group.class);
+        group.setGroupLeader(doctorDao.getReferenceById(docId));
+        doctorDao.getReferenceById(docId).setGroup(group);
+        groupDao.save(group);
     }
 
     @Override
     public List<Doctor> getDocs() {
-        return null;
+        return doctorDao.findAll();
     }
 
     @Override
     public void deleteGroup(Long docId) {
-
+        groupDao.delete(doctorDao.getReferenceById(docId).getGroup());
+        doctorDao.getReferenceById(docId).setGroup(null);
     }
 
     @Override
     public GroupDTO viewGroup(Long docId) {
-        return null;
+        Group group = doctorDao.findById(docId).orElseThrow().getGroup();
+        return mapper.map(group, GroupDTO.class);
     }
 }
